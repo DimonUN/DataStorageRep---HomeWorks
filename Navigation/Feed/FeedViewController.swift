7d372b2717207318be776b468d4c4abc13890e8e
DimonUN <nikonorov_photo@mail.ru>
2022-06-21 15:34:50 +0300

    Выполнены условия ДЗ

--- Navigation/Feed/FeedViewController.swift
+++ Navigation/Feed/FeedViewController.swift
@@ -2,71 +2,41 @@ import UIKit
 
 class FeedViewController: UIViewController {
     
-    var post1: Post = {
-        let post1 = Post(title: "", author: "Ignat", description: "About the dangers of malnutrition.", image: "post1", likes: 15, views: 581)
-        return post1
-    }()
     
-    
-    var firstButton: UIButton = {
-        let firstButton = UIButton()
-        firstButton.backgroundColor = .systemBlue
-        firstButton.setTitle("Button 1", for: .normal)
-        firstButton.setTitleColor(.white, for: .normal)
-        firstButton.layer.cornerRadius = 20
-        firstButton.layer.borderWidth = 1
-        firstButton.layer.borderColor = UIColor.gray.cgColor
-        firstButton.addTarget(self,
-                              action: #selector(buttonAction),
-                              for: .touchUpInside)
-        return firstButton
-    }()
-    
-    
-    var secondButton: UIButton = {
-        let secondButton = UIButton()
-        secondButton.backgroundColor = .systemCyan
-        secondButton.setTitle("Button 2", for: .normal)
-        secondButton.setTitleColor(.white, for: .normal)
-        secondButton.layer.cornerRadius = 20
-        secondButton.layer.borderWidth = 1
-        secondButton.layer.borderColor = UIColor.gray.cgColor
-        secondButton.addTarget(self,
-                               action: #selector(buttonAction),
-                               for: .touchUpInside)
-        return secondButton
-    }()
+    var networkRequest: UIButton = {
+        let networkRequest = UIButton(type: .system)
+        networkRequest.toAutoLayout()
+        networkRequest.backgroundColor = .systemBlue
+        networkRequest.setTitle("Create request", for: .normal)
+        networkRequest.setTitleColor(.white, for: .normal)
+        networkRequest.layer.cornerRadius = 20
+        
+        networkRequest.addTarget(
+            self,
+            action: #selector(buttonAction),
+            for: .touchUpInside
+        )
 
-    var stackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.distribution = .equalSpacing
-        stackView.alignment = .center
-        stackView.spacing = 10
-        stackView.toAutoLayout()
-        return stackView
+        return networkRequest
     }()
+    
+    
 
-    fileprivate func setupConstraints() {
+
+
+    private func setupConstraints() {
         NSLayoutConstraint.activate([
-            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            stackView.heightAnchor.constraint(equalToConstant: 150),
-            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-
-            firstButton.heightAnchor.constraint(equalToConstant: 50),
-            firstButton.widthAnchor.constraint(equalToConstant: 100),
-
-            secondButton.heightAnchor.constraint(equalToConstant: 50),
-            secondButton.widthAnchor.constraint(equalToConstant: 100)
+            networkRequest.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            networkRequest.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            networkRequest.widthAnchor.constraint(equalToConstant: 150.0),
+            networkRequest.heightAnchor.constraint(equalToConstant: 50.0)
         ])
     }
 
     private func setupUI() {
         view.backgroundColor = .white
         title = "Your feed"
-        stackView.addSubviews(firstButton, secondButton)
-        view.addSubview(stackView)
+        view.addSubview(networkRequest)
     }
     
     override func viewDidLoad() {
@@ -76,9 +46,6 @@ class FeedViewController: UIViewController {
     }
 
     @objc func buttonAction(sender: UIButton) {
-        let vc = PostViewController()
-        vc.firstPost = self.post1.title
-        self.navigationController?.pushViewController(vc, animated: true)
-     }
+        let _ = NetworkService()
+    }
 }
-
